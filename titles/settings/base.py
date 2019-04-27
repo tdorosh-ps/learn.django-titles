f7ddc2cc7b31c7b79647ff01556dee263fe90a03
tdorosh <tdtechmail@ukr.net>
2019-04-27 13:31:57 +0300

    add title to installed apps

--- titles/settings/base.py
+++ titles/settings/base.py
@@ -20,13 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY_TESTING']
+SECRET_KEY = os.environ['SECRET_KEY_TLT']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # Application definition
 
 INSTALLED_APPS = [
+	'title.apps.TitleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
