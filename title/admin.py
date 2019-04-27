7a4633d4619b2aec85699daf0b69a7258d0273f2
tdorosh <tdtechmail@ukr.net>
2019-04-27 16:47:19 +0300

    register models in admin.py

--- title/admin.py
+++ title/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Title, IncomingLetter, OutgoingLetter, Counterparty
 # Register your models here.
+admin.site.register(Title)
+admin.site.register(IncomingLetter)
+admin.site.register(OutgoingLetter)
+admin.site.register(Counterparty)
