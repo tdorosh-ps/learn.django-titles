c3413dda2a3b9bfcc128192bc40777dd9cdee12f
tdorosh <tdtechmail@ukr.net>
2019-08-25 10:27:49 +0300

    add initDone js function

--- title/static/js/main.js
+++ title/static/js/main.js
@@ -1,6 +1,14 @@
 function initDone() {
-	$('#done').click(function(event) {
-		alert('test');
+	$('input[type="checkbox"]').click(function(event) {
+		var box = $(this);
+		$.ajax(box.data('url'), {
+			'type': 'GET',
+			'async': true,
+			'dataType': 'json',
+			'data': {
+				'done': box.is(':checked') ? 'on': ''
+			}
+		})
 	});
 }
 
